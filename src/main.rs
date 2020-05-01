99f0aec69bbf7faced20ff15911239a9df2bbe1d
Adam Melnyk <adam@adammelnyk.ca>
2020-04-30 21:53:18 -0500

    More commands + help docs
    * Added Volume, set volume
    * Added docs and instructions for each command

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,69 @@ use std::time::Duration;
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
+#[structopt(rename_all = "kebab-case")]
 enum Cli {
+    #[structopt(
+        about = "Displays info about all rooms",
+        help = "USAGE: info"
+    )]
     Info,
+    #[structopt(
+        about = "Stops specified room",
+        help = "USAGE: stop MyRoomName"
+    )]
     Stop {
         name: String,
     },
+    #[structopt(
+        about = "Plays specified room",
+        help = "USAGE: play MyRoomName"
+    )]
     Play {
         name: String,
     },
+    #[structopt(
+        about = "Pauses a specified room",
+        help = "USAGE: volume MyRoomName"
+    )]
     Pause {
         name: String,
     },
+    #[structopt(
+        about = "Skips to the next track for a specified room",
+        help = "USAGE: next MyRoomName"
+    )]
     Next {
         name: String,
     },
+    #[structopt(
+        about = "skips back to the previous track for a specified room",
+        help = "USAGE: previous MyRoomName")]
     Previous {
-        name: String
+        name: String,
     },
+    #[structopt(
+        about = "Displays the volume for a specified room",
+        help = "USAGE: volume MyRoomName"
+    )]
+    Track {
+        name: String,
+    },
+    #[structopt(
+        about = "Displays the volume for a specified room",
+        help = "USAGE: volume MyRoomName"
+    )]
+    Volume {
+        name: String,
+    },
+    #[structopt(
+        about = "Sets the volume for a specified room",
+        help = "USAGE: set-volume MyRoomName 11"
+    )]
+    SetVolume {
+        name: String,
+        volume: u16,
+    }
 }
 
 #[tokio::main]
@@ -31,7 +77,10 @@ async fn main() -> Result<(), sonor::Error> {
         Cli::Play { name } => play(name).await,
         Cli::Pause { name } => pause(name).await,
         Cli::Next { name } => next(name).await,
-        Cli::Previous { name }=> previous(name).await,
+        Cli::Previous { name } => previous(name).await,
+        Cli::Track { name } => track(name).await,
+        Cli::Volume { name } => volume(name).await,
+        Cli::SetVolume { name, volume} => set_volume(name, volume).await,
     }
 }
 
@@ -62,24 +111,50 @@ async fn stop(name: String) -> Result<(), sonor::Error> {
 
 async fn play(name: String) -> Result<(), sonor::Error> {
     let speaker = sonor::find(&name, Duration::from_secs(2)).await?
-    .expect("room exists");
+        .expect("room exists");
     return speaker.play().await;
 }
 
 async fn pause(name: String) -> Result<(), sonor::Error> {
     let speaker = sonor::find(&name, Duration::from_secs(2)).await?
-    .expect("room exists");
+        .expect("room exists");
     return speaker.pause().await;
 }
 
 async fn next(name: String) -> Result<(), sonor::Error> {
     let speaker = sonor::find(&name, Duration::from_secs(2)).await?
-    .expect("room exists");
+        .expect("room exists");
     return speaker.next().await;
 }
 
 async fn previous(name: String) -> Result<(), sonor::Error> {
     let speaker = sonor::find(&name, Duration::from_secs(2)).await?
-    .expect("room exists");
+        .expect("room exists");
     return speaker.previous().await;
+}
+
+async fn track(name: String) -> Result<(), sonor::Error> {
+    let speaker = sonor::find(&name, Duration::from_secs(2)).await?
+        .expect("room exists");
+    match speaker.track().await? {
+        Some(track_info) => println!("- Currently playing '{} on '{}", track_info.track(), name),
+        None => println!("- No track currently playing on {}", name),
+    }
+    Ok(())
+}
+
+async fn volume(name: String) -> Result<(), sonor::Error> {
+    let speaker = sonor::find(&name, Duration::from_secs(2)).await?
+        .expect("room exists");
+    match speaker.volume().await {
+        Ok(vol) => println!("The volume is currently at {} on {}", vol, name),
+        Err(_) => println!("Error"),
+    }
+    Ok(())
+}
+
+async fn set_volume(name: String, volume: u16) -> Result<(), sonor::Error> {
+    let speaker = sonor::find(&name, Duration::from_secs(2)).await?
+        .expect("room exists");
+    return speaker.set_volume(volume).await;
 }
